cad5063dda97017e35b046c284fd3b6ddc4aa381
CanftIn <wwc7033@gmail.com>
2023-09-07 13:47:19 +0800

    update token and def

--- include/Radium/Parse/Token.h
+++ include/Radium/Parse/Token.h
@@ -3,54 +3,114 @@
 
 #include <cstdint>
 
+#include "Radium/Basic/LLVM.h"
+#include "Radium/Basic/SourceLoc.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/SMLoc.h"
 
 namespace Radium {
 
-namespace Tok {
-
 enum class TokenKind : uint8_t {
-  Unknown = 0,
-  Eof,
-  Identifier,
-  Numeric_Constant,
+  unknown = 0,
+  eof,
+  code_complete,
+  identifier,
+  oper_binary,
+  oper_postfix,
+  oper_prefix,
+  dollarident,
+  integer_literal,
+  floating_literal,
+  string_literal,
+  character_literal,
+  sil_local_name,  // %42 in RIL mode.
+  comment,
 
-#define RADIUM_KEYWORD(Name) KW_##Name,
+#define RADIUM_KEYWORD(Name) kw_##Name,
 #define RADIUM_PUNCTUATOR(X, Y) X,
-#include "Radium/Parse/TokenKind.def"
+#include "Radium/Parse/Tokens.def"
 
-  NUM_TOKENS,
+  num_tokens,
 };
-
-}  // namespace Tok
 
 class Token {
  public:
-  auto GetKind() const -> Tok::TokenKind { return kind_; }
+  Token() : kind_(TokenKind::num_tokens), at_start_of_line_(false) {}
 
-  void Setkind(Tok::TokenKind k) { kind_ = k; }
+  auto getKind() const -> TokenKind { return kind_; }
 
-  auto Is(Tok::TokenKind k) const -> bool { return kind_ == k; }
-  auto IsNot(Tok::TokenKind k) const -> bool { return kind_ != k; }
+  void setKind(TokenKind kind) { kind_ = kind; }
+
+  auto is(TokenKind kind) const -> bool { return kind_ == kind; }
+
+  auto isNot(TokenKind kind) const -> bool { return kind_ != kind; }
+
+  auto isAnyOperator() const -> bool {
+    return kind_ == TokenKind::oper_binary ||
+           kind_ == TokenKind::oper_postfix || kind_ == TokenKind::oper_prefix;
+  }
+
+  auto isNotAnyOperator() const -> bool { return !isAnyOperator(); }
 
   auto GetLocation() const -> llvm::SMLoc {
     return llvm::SMLoc::getFromPointer(text_.begin());
   }
 
-  auto GetText() const -> llvm::StringRef { return text_; }
-  void SetText(llvm::StringRef t) { text_ = t; }
+  auto isAtStartOfLine() const -> bool { return at_start_of_line_; }
 
-  auto GetLength() const -> unsigned { return text_.size(); }
+  void setAtStartOfLine(bool value) { at_start_of_line_ = value; }
 
-  void SetToken(Tok::TokenKind k, llvm::StringRef t) {
-    kind_ = k;
-    text_ = t;
+  auto isContextualKeyword(llvm::StringRef context_kw) const -> bool {
+    return is(TokenKind::identifier) && text_ == context_kw;
+  }
+
+  auto isContextualPunctuator(llvm::StringRef context_punc) const -> bool {
+    return is(TokenKind::oper_binary) && text_ == context_punc;
+  }
+
+  auto isFollowingLParen() const -> bool {
+    return !isAtStartOfLine() && kind_ == TokenKind::l_paren;
+  }
+
+  auto isFollowingLSquare() const -> bool {
+    return !isAtStartOfLine() && kind_ == TokenKind::l_square;
+  }
+
+  auto isFollowingLBrace() const -> bool {
+    return !isAtStartOfLine() && kind_ == TokenKind::l_brace;
+  }
+
+  auto isKeyword() const -> bool {
+    switch (kind_) {
+#define RADIUM_KEYWORD(X) \
+  case TokenKind::kw_##X: \
+    return true;
+#include "Radium/Parse/Tokens.def"
+      default:
+        return false;
+    }
+  }
+
+  auto getLoc() const -> SourceLoc {
+    return SourceLoc(llvm::SMLoc::getFromPointer(text_.begin()));
+  }
+
+  auto getText() const -> llvm::StringRef { return text_; }
+
+  void setText(llvm::StringRef text) { text_ = text; }
+
+  auto getLength() const -> unsigned { return text_.size(); }
+
+  void setToken(TokenKind kind, llvm::StringRef text) {
+    kind_ = kind;
+    text_ = text;
   }
 
  private:
-  Tok::TokenKind kind_;
-
+  // token的类型
+  TokenKind kind_;
+  // 是否token是在行首
+  bool at_start_of_line_;
+  // token所在的source buffer中实际的文本
   llvm::StringRef text_;
 };
 
